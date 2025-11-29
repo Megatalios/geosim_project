6ade761f39e4a79741947fe66c60f891a99873ae
Kamil <kamil.nabyev2003@gmail.com>
2025-11-29 19:45:41 +0500

    Перед добавлением аналитически вычисленной части

--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -1,1 +1,17 @@
 #include "Sphere.h"
+
+Eigen::Vector3d Sphere::get3Dpoint(double u, double v) const {
+}
+
+Eigen::Matrix2d Sphere::metricTensor(double u, double v) const {
+}
+
+Eigen::Matrix2d Sphere::inverseMetricTensor(double u, double v) const {
+}
+
+double Sphere::metricTensorDet(double u, double v) const {
+}
+
+std::array<Eigen::Matrix2d, 2> Sphere::cristoffelSymbols(double u, double v) const {
+}
+
--- src/Sphere.h
+++ src/Sphere.h
@@ -1,2 +1,13 @@
 #include "ISurface.h"
 
+class Sphere : public ISurface {
+public:
+    ///<summary>
+    ///
+    ///</summary>
+    Eigen::Vector3d get3Dpoint(double u, double v) const override;
+    Eigen::Matrix2d metricTensor(double u, double v) const override;
+    Eigen::Matrix2d inverseMetricTensor(double u, double v) const override;
+    double metricTensorDet(double u, double v) const override;
+    std::array<Eigen::Matrix2d, 2> cristoffelSymbols(double u, double v) const override;
+};
